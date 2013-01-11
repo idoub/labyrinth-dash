93444b6b3f5dfebcb4a11e14899b846153464391
idoub <isaac.doub@gmail.com>
2013-01-11 17:50:18 +0000

    Trying to get my changes back after stashing

--- gen/com/labyrinthdash/R.java
+++ gen/com/labyrinthdash/R.java
@@ -8,6 +8,9 @@
 package com.labyrinthdash;
 
 public final class R {
+    public static final class anim {
+        public static final int playerjump=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -47,21 +50,21 @@ public final class R {
         public static final int spaceblack=0x7f020021;
     }
     public static final class id {
-        public static final int name=0x7f070000;
-        public static final int player=0x7f070001;
+        public static final int name=0x7f080000;
+        public static final int player=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int menu_settings=0x7f040001;
-        public static final int title_activity_main=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int menu_settings=0x7f050001;
+        public static final int title_activity_main=0x7f050002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
