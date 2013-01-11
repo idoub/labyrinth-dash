c5a2e907d922972a5409e99ee1efafd03be3b277
Matthew <matthew.woodacre@gmail.com>
2013-01-11 21:31:01 +0000

    Full scoring

--- gen/com/labyrinthdash/R.java
+++ gen/com/labyrinthdash/R.java
@@ -51,6 +51,9 @@ public final class R {
         public static final int single_button=0x7f020022;
         public static final int space=0x7f020023;
         public static final int spaceblack=0x7f020024;
+        public static final int target=0x7f020025;
+        public static final int target2=0x7f020026;
+        public static final int target3=0x7f020027;
     }
     public static final class id {
         public static final int name=0x7f080000;
